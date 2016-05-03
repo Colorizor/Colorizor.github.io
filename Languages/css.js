42ceeed16415ea5d9337b5a965cafc4220a9027d
Alexander Hovy <colorizor@gmail.com>
2016-05-03 17:38:33 +0200

    Update css.js

--- Languages/css.js
+++ Languages/css.js
@@ -3,13 +3,13 @@
   //---------------------------------------------------RegEx----------------------------------------------------
   //------------------------------------------------------------------------------------------------------------
   var regex = [
-    ['<span comment>$&</span>', /\/\*([\s\S]*?)\*\//igm],
-    ['<span value>$&</span>', /(\'|\")([\s\S]*?)(\'|\")/igm],
-    ['<span reserved>$&</span>', /([\w\d\.\#].+)(?=\{)/igm],
-    ['<span selector>$&</span>', /([\w\d\-\[\]\(\)]+)(?!.*?(\{))(?=\:)/igm],
-    ['<span attribute>$&</span>', /(?:[^\:\W\d])([\w]+)(?=(\;|\,))/igm],
-    ['<span digit>$&</span>', /([\d\.]+)(em|ex|\%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/igm],
-    ['<a id="link" href="$&" target="_blank">$&</a>', /(ftp|http|https)\:\/\/([\w\d\W]*?)(?=[\s\'\"\(\)\{\}\[\]])/igm],
+    ['<span class="comment">$&</span>', /\/\*([\s\S]*?)\*\//igm],
+    ['<span class="value">$&</span>', /(\'|\")([\s\S]*?)(\'|\")/igm],
+    ['<span class="reserved">$&</span>', /([\w\d\.\#].+)(?=\{)/igm],
+    ['<span class="selector">$&</span>', /([\w\d\-\[\]\(\)]+)(?!.*?(\{))(?=\:)/igm],
+    ['<span class="attribute">$&</span>', /(?:[^\:\W\d])([\w]+)(?=(\;|\,))/igm],
+    ['<span class="digit">$&</span>', /([\d\.]+)(em|ex|\%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/igm],
+    ['<a class="link" href="$&" target="_blank">$&</a>', /(ftp|http|https)\:\/\/([\w\d\W]*?)(?=[\s\'\"\(\)\{\}\[\]])/igm],
     ['<span style="color: $&;">$&</span>', /((rgba|rgb|hsla|hsl)\(([\s\S]*?)\)|\#([\w\d]){6})(?!.*?\{)/igm]
   ];
   
