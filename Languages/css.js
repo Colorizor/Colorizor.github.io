20d6be6d71a76f3edbd13957457473c96f7c9608
Alexander Hovy <colorizor@gmail.com>
2016-05-03 16:56:57 +0200

    Update css.js

--- Languages/css.js
+++ Languages/css.js
@@ -3,12 +3,12 @@
   //---------------------------------------------------RegEx----------------------------------------------------
   //------------------------------------------------------------------------------------------------------------
   var regex = [
-    ['<span id="comment">$&</span>', /\/\*([\s\S]*?)\*\//igm],
-    ['<span id="value">$&</span>', /(\'|\")([\s\S]*?)(\'|\")/igm],
-    ['<span id="reserved">$&</span>', /([\w\d\.\#].+)(?=\{)/igm],
-    ['<span id="selector">$&</span>', /([\w\d\-\[\]\(\)]+)(?!.*?(\{))(?=\:)/igm],
-    ['<span id="attribute">$&</span>', /(?:[^\:\W\d])([\w]+)(?=(\;|\,))/igm],
-    ['<span id="digit">$&</span>', /([\d\.]+)(em|ex|\%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/igm],
+    ['<span comment>$&</span>', /\/\*([\s\S]*?)\*\//igm],
+    ['<span value>$&</span>', /(\'|\")([\s\S]*?)(\'|\")/igm],
+    ['<span reserved>$&</span>', /([\w\d\.\#].+)(?=\{)/igm],
+    ['<span selector>$&</span>', /([\w\d\-\[\]\(\)]+)(?!.*?(\{))(?=\:)/igm],
+    ['<span attribute>$&</span>', /(?:[^\:\W\d])([\w]+)(?=(\;|\,))/igm],
+    ['<span digit>$&</span>', /([\d\.]+)(em|ex|\%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/igm],
     ['<a id="link" href="$&" target="_blank">$&</a>', /(ftp|http|https)\:\/\/([\w\d\W]*?)(?=[\s\'\"\(\)\{\}\[\]])/igm],
     ['<span style="color: $&;">$&</span>', /((rgba|rgb|hsla|hsl)\(([\s\S]*?)\)|\#([\w\d]){6})(?!.*?\{)/igm]
   ];
@@ -16,7 +16,7 @@
   //------------------------------------------------------------------------------------------------------------
   //-------------------------------------------------COLORIZING-------------------------------------------------
   //------------------------------------------------------------------------------------------------------------
-  $.each($('pre[language="css"]'), function() {
+  $.each($('code[language="css"]'), function() {
     //--------------------------------------------------FETCH
     var str = $(this).html();
     //--------------------------------------------------SIZING
@@ -33,49 +33,4 @@
     //--------------------------------------------------SAVE
     $(this).html(str);
   });
-  
-  /*//------------------------------------------------------------------------------------------------------------
-  //---------------------------------------------------SPLIT----------------------------------------------------
-  //------------------------------------------------------------------------------------------------------------
-  $.each($('pre[language="css"]'), function() {
-    var str = $(this).html();
-    str = str.replace(/([\s\S]+)/igm, '<span id="all-number"></span><span id="all-code">$&</span>');
-    $(this).html(str);
-  });
-  
-  //------------------------------------------------------------------------------------------------------------
-  //--------------------------------------------------NUMBERING-------------------------------------------------
-  //------------------------------------------------------------------------------------------------------------
-  $.each($('pre[language="css"]').find('span[id="all-code"]'), function(line) {
-    $(this).html(function(index, html) {
-      return html.replace(/(^\n|.+)/igm, '<span id="code">$&</span>');
-    });
-    
-    line = 0;
-    
-    $.each($(this).find('span[id="code"]'), function() {
-      line++;
-      var str = $($(this).parent().parent().find('span[id="all-number"]')).html();
-      $($(this).parent().parent().find('span[id="all-number"]')).html(str + '<span id="number">' + line + '</span>\n');
-    });
-  });
-  
-  //------------------------------------------------------------------------------------------------------------
-  //--------------------------------------------------FEATURES--------------------------------------------------
-  //------------------------------------------------------------------------------------------------------------
-  $('pre[language="css"]').find('span[id="all-code"]').click(function() {
-    var range, selection;
-
-    if (window.getSelection && document.createRange) {
-      selection = window.getSelection();
-      range = document.createRange();
-      range.selectNodeContents($(this)[0]);
-      selection.removeAllRanges();
-      selection.addRange(range);
-    } else if (document.selection && document.body.createTextRange) {
-      range = document.body.createTextRange();
-      range.moveToElementText($(this)[0]);
-      range.select();
-    }
-  });*/
 })();
