d18cb1deea16e685595cf7a06613a6934602def2
Alexander Hovy <colorizor@gmail.com>
2016-05-04 17:15:12 +0200

    Update css.js

--- Languages/css.js
+++ Languages/css.js
@@ -7,9 +7,9 @@
     ['<span class="value">$&</span>', /(\'|\")([\s\S]*?)(\'|\")/igm],
     ['<span class="reserved">$&</span>', /(?:(?!.*?[\{]))([\w\-]+)(?=[\:])/igm],
     ['<span class="selector">$&</span>', /([\#\.\w\-]+)(?:((?!.*?[\;\}])|(?=.*?[\{])))/igm],
-    ['<span class="attribute">$&</span>', /(([^\w\:]|[\w])+)(?=[\;\}])/igm],
+    ['<span class="attribute">$&</span>', /(([^\w\:]|[\w])+)(?=[\;\}])/igm]//,
     //['<span class="digit">$&</span>', /([\d\.]+)(em|ex|\%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/igm],
-    //['<a class="link" href="$&" target="_blank">$&</a>', /(ftp|http|https)\:\/\/([\w\d\W]*?)(?=[\s\'\"\(\)\{\}\[\]])/igm]//,
+    //['<a class="link" href="$&" target="_blank">$&</a>', /(ftp|http|https)\:\/\/([\w\d\W]*?)(?=[\s\'\"\(\)\{\}\[\]])/igm],
     //['<span style="color: $&;">$&</span>', /((rgba|rgb|hsla|hsl)\(([\s\S]*?)\)|\#([\w\d]){6})(?!.*?\{)/igm]
   ];
   
