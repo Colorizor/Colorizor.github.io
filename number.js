b45a9f3316abfe6b8586afe81f3aa2578c4fd9db
Alexander Hovy <colorizor@gmail.com>
2016-05-04 08:13:39 +0200

    Create number.js

---
+++ number.js
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------------------------------------
+      //---------------------------------------------------NUMBERING----------------------------------------------------
+      //------------------------------------------------------------------------------------------------------------
+      $.each($('pre[number]').find('code'), function() {
+        var str = $(this).html();
+        str = str.replace(/([\s\S]+)/igm, '<span class="numbers"></span><span class="coding">$&</span>');
+        $(this).html(str);
+      });
+      
+      $.each($('pre[number]').find('span[class="coding"]'), function(line) {
+        $(this).html(function(index, html) {
+          return html.replace(/(^\n|.+)/igm, '<span class="code">$&</span>');
+        });
+    
+        line = 0;
+    
+        $.each($(this).find('span[class="code"]'), function() {
+          line++;
+          var str = $($(this).parent().parent().find('span[class="numbers"]')).html();
+          $($(this).parent().parent().find('span[class="numbers"]')).html(str + '<span class="number">' + line + '</span>\n');
+        });
+      });
