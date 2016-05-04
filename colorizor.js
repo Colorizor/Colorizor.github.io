71ea6895dfa3e7857c17da4f3ba3989c84132b4b
Alexander Hovy <colorizor@gmail.com>
2016-05-04 08:13:07 +0200

    Update colorizor.js

--- colorizor.js
+++ colorizor.js
@@ -76,34 +76,11 @@
           loadCSS('https://colorizor.github.io/Themes/' + theme.toLowerCase() + '.css');
         }
       });
-      
-      //------------------------------------------------------------------------------------------------------------
-      //---------------------------------------------------NUMBERING----------------------------------------------------
-      //------------------------------------------------------------------------------------------------------------
-      $.each($('pre[number]').find('code'), function() {
-        var str = $(this).html();
-        str = str.replace(/([\s\S]+)/igm, '<span class="numbers"></span><span class="coding">$&</span>');
-        $(this).html(str);
-      });
-      
-      $.each($('pre[number]').find('span[class="coding"]'), function(line) {
-        $(this).html(function(index, html) {
-          return html.replace(/(^\n|.+)/igm, '<span class="code">$&</span>');
-        });
-    
-        line = 0;
-    
-        $.each($(this).find('span[class="code"]'), function() {
-          line++;
-          var str = $($(this).parent().parent().find('span[class="numbers"]')).html();
-          $($(this).parent().parent().find('span[class="numbers"]')).html(str + '<span class="number">' + line + '</span>\n');
-        });
-      });
   
       //------------------------------------------------------------------------------------------------------------
       //--------------------------------------------------FEATURES--------------------------------------------------
       //------------------------------------------------------------------------------------------------------------
-      ($('code').find('span[class="coding"]') || $('code')).click(function() {
+      $('code').click(function() {
         var range, selection;
 
         if (window.getSelection && document.createRange) {
