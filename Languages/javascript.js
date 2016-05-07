e440f6fe8f7c9d233718aaf941563913533108b4
Alexander Hovy <colorizor@gmail.com>
2016-05-07 20:07:05 +0200

    Update javascript.js

--- Languages/javascript.js
+++ Languages/javascript.js
@@ -12,7 +12,7 @@
     ['<span class="reserved">$&</span>', /\b(alert|all|anchor|anchors|area|assign|blur|button|checkbox|clearInterval|clearTimeout|clientInformation|close|closed|confirm|constructor|crypto|decodeURI|decodeURIComponent|defaultStatus|document|element|elements|embed|embeds|encodeURI|encodeURIComponent|escape|event|fileUpload|focus|form|forms|frame|innerHeight|innerWidth|layer|layers|link|location|mimeTypes|navigate|navigator|frames|frameRate|hidden|history|image|images|offscreenBuffering|open|opener|option|outerHeight|outerWidth|packages|pageXOffset|pageYOffset|parent|parseFloat|parseInt|password|plugin|prompt|propertyIsEnum|radio|reset|screenX|screenY|scroll|secure|select|self|setInterval|setTimeout|status|submit|taint|text|textarea|top|unescape|untaint|window)\b/igm],
     ['<span class="parameter">$&</span>', /\b(Array|Date|eval|function|hasOwnProperty|Infinity|isFinite|isNaN|isPrototypeOf|length|Math|NaN|name|Number|Object|prototype|String|toString|undefined|valueOf)\b/igm],
     ['<span class="parameter">$&</span>', /\b(onblur|onclick|onerror|onfocus|onkeydown|onkeypress|onkeyup|onmouseover|onload|onmouseup|onmousedown|onsubmit)\b/igm],
-    ['<span class="selector">$&</span>', /(?:(?=.*?[\;]))([\w]+)(?=[\(])/igm],
+    ['<span class="selector">$&</span>', /([\w]+)(?=[\(])/igm],
     ['<span class="comment">$&</span>', /\/\/.+/igm],
     ['<span class="unit">$&</span>', /([\d]|\-[\d]|\.\d)+/igm],
     //Clean
