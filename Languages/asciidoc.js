c8379ff1a2ebc182603777e62c0d98ae96a9a2da
Alexander Hovy <colorizor@gmail.com>
2016-05-08 20:29:04 +0200

    Create asciidoc.js

---
+++ Languages/asciidoc.js
@@ -0,0 +1,50 @@
+(function() {
+  //------------------------------------------------------------------------------------------------------------
+  //---------------------------------------------------RegEx----------------------------------------------------
+  //------------------------------------------------------------------------------------------------------------
+  var regex = [
+    //Problem Characters
+    ['\\$&\/', /[\'\"\<\>]/igm],
+    ['\:\/\\\/', /\:\/\//igm],
+    //Syntax
+    ['<span id="value">$&</span>', /(\\\'\/(.*?)\\\'\/|\\\"\/(.*?)\\\"\/)/igm],
+    ['<span id="attribute">$&</span>', /\b(abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\b/igm],
+    ['<span id="reserved">$&</span>', /\b(alert|all|anchor|anchors|area|assign|blur|button|checkbox|clearInterval|clearTimeout|clientInformation|close|closed|confirm|constructor|crypto|decodeURI|decodeURIComponent|defaultStatus|document|element|elements|embed|embeds|encodeURI|encodeURIComponent|escape|event|fileUpload|focus|form|forms|frame|height|innerHeight|innerWidth|layer|layers|link|location|mimeTypes|navigate|navigator|frames|frameRate|hidden|history|image|images|offscreenBuffering|open|opener|option|outerHeight|outerWidth|packages|pageXOffset|pageYOffset|parent|parseFloat|parseInt|password|plugin|prompt|propertyIsEnum|radio|reset|screen|screenX|screenY|scroll|secure|select|self|setInterval|setTimeout|status|submit|taint|text|textarea|top|unescape|untaint|width|window)\b/igm],
+    ['<span id="parameter">$&</span>', /\b(Array|Date|eval|function|hasOwnProperty|Infinity|isFinite|isNaN|isPrototypeOf|length|Math|NaN|name|Number|Object|prototype|String|toString|undefined|valueOf)\b/igm],
+    ['<span id="parameter">$&</span>', /\b(onblur|onclick|onerror|onfocus|onkeydown|onkeypress|onkeyup|onmouseover|onload|onmouseup|onmousedown|onsubmit)\b/igm],
+    ['<span id="selector">$&</span>', /([\w]+)(?=[\(])/igm],
+    ['<span id="comment">$&</span>', /(^\/{4,}$)([\s\S]+)(^\/{4,}$)/igm],
+    ['<span id="comment">$&</span>', /\/\/.+/igm],
+    ['<span id="unit">$&</span>', /([\d]|\-[\d]|\.\d)+/igm],
+    //Clean
+    ['', /(?:(?!([\s\S]+)(^\/{4,}$)))(\<span(.*?)\>|\<\/span\>)(?=([\s\S]*?)(^\/{4,}$))/igm],
+    ['', /(?:(?!.*?\/\/))(\<span(.*?)\>|\<\/span\>)(?=(.*?)\<\/span\>$)/igm],
+    ['', /(?:(?!.*?\\\'\/.+\'\/))(\<span(.*?)\>|\<\/span\>)(?=.*?\\\'\/)/igm],
+    ['', /(?:(?!.*?\\\"\/.+\"\/))(\<span(.*?)\>|\<\/span\>)(?=.*?\\\"\/)/igm],
+    //Fix Characters
+    ['\'', /\\\'\//igm],
+    ['\"', /\\\"\//igm],
+    ['&lt;', /\\\<\//igm],
+    ['&gt;', /\\\>\//igm],
+    ['\:\/\/', /\:\/\\\//igm]
+  ];
+  
+  //------------------------------------------------------------------------------------------------------------
+  //-------------------------------------------------COLORIZING-------------------------------------------------
+  //------------------------------------------------------------------------------------------------------------
+  $.each($('code[language="asciidoc"]'), function() {
+    //--------------------------------------------------SIZING
+    $(this).css({
+      'height': 'auto',
+      'left': '0px',
+      'right': '0px',
+      'width': 'auto'
+    });
+    //--------------------------------------------------FIND
+    for (a = 0; a < regex.length; a++) {
+      var str = $(this).html();
+      str = str.replace(regex[a][1], regex[a][0]);
+      $(this).html(str);
+    }
+  });
+})();
