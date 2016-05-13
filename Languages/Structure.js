a03aa2b9860b4ef0528ad2e703f86bbaeb28d071
Alexander Hovy <colorizor@gmail.com>
2016-05-13 13:03:18 +0200

    Update Structure.js

--- Languages/Structure.js
+++ Languages/Structure.js
@@ -1,1 +1,12 @@
-var Structure = new Initialize();
+(function() {
+  var Structure = new Initialize({
+    language: 'structure',
+    prepare: [
+    ],
+    execute: [
+    ],
+    finalise: [
+    ]
+  });
+  Structure.Colorize();
+})();
