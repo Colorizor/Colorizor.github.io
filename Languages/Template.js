f5c1e00fbde5aea2f7375eebfd98cf5738e7eca9
Alexander Hovy <colorizor@gmail.com>
2016-05-13 15:58:06 +0200

    Update Template.js

--- Languages/Template.js
+++ Languages/Template.js
@@ -2,10 +2,42 @@
   const data = {
     language: '',
     prepare: [
+      {
+        pat: /[\'\"]/igm,
+        rep: '\\$&\/'
+      },
+      {
+        pat: /\:\/\//igm,
+        rep: '\:\/\\\/'
+      }
     ],
     execute: [
+      {
+        name: '',
+        begin: {
+          pat: //igm,
+          exclude: true
+        },
+        end: {
+          pat: //igm,
+          exclude: true
+        },
+        pattern: //igm
+      }
     ],
     finalise: [
+      {
+        pat: /\\\'\//igm,
+        rep: '\''
+      },
+      {
+        pat: /\\\"\//igm,
+        rep: '\"'
+      },
+      {
+        pat: /\:\/\\\//igm,
+        rep: '\:\/\/'
+      }
     ]
   };
   clz.Colorize(data);
