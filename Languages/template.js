c8ecbfefe4415b2420e4babfeb7eaf9d488fb93f
Alexander Hovy <colorizor@gmail.com>
2016-05-15 20:13:40 +0200

    Update template.js

--- Languages/template.js
+++ Languages/template.js
@@ -9,32 +9,32 @@
     ],
     execute: [
       {
-        custom: /(\'(.*?)\'|\"(.*?)\")/igm,
+        custom: '([\'](.*?)[\']|[\"](.*?)[\"])',
         pat: /.+/igm,
         rep: '<span id="value">$&</span>'
       },
       {
-        custom: /\b(abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\b/igm,
+        custom: '\\b(abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\\b',
         pat: /.+/igm,
         rep: '<span id="attribute">$&</span>'
       },
       {
-        custom: /\b(alert|all|anchor|anchors|area|assign|blur|button|checkbox|clearInterval|clearTimeout|clientInformation|close|closed|confirm|constructor|crypto|decodeURI|decodeURIComponent|defaultStatus|document|element|elements|embed|embeds|encodeURI|encodeURIComponent|escape|event|fileUpload|focus|form|forms|frame|height|innerHeight|innerWidth|layer|layers|link|location|mimeTypes|navigate|navigator|frames|frameRate|hidden|history|image|images|offscreenBuffering|open|opener|option|outerHeight|outerWidth|packages|pageXOffset|pageYOffset|parent|parseFloat|parseInt|password|plugin|prompt|propertyIsEnum|radio|reset|screen|screenX|screenY|scroll|secure|select|self|setInterval|setTimeout|status|submit|taint|text|textarea|top|unescape|untaint|width|window)\b/igm,
+        custom: '\\b(alert|all|anchor|anchors|area|assign|blur|button|checkbox|clearInterval|clearTimeout|clientInformation|close|closed|confirm|constructor|crypto|decodeURI|decodeURIComponent|defaultStatus|document|element|elements|embed|embeds|encodeURI|encodeURIComponent|escape|event|fileUpload|focus|form|forms|frame|height|innerHeight|innerWidth|layer|layers|link|location|mimeTypes|navigate|navigator|frames|frameRate|hidden|history|image|images|offscreenBuffering|open|opener|option|outerHeight|outerWidth|packages|pageXOffset|pageYOffset|parent|parseFloat|parseInt|password|plugin|prompt|propertyIsEnum|radio|reset|screen|screenX|screenY|scroll|secure|select|self|setInterval|setTimeout|status|submit|taint|text|textarea|top|unescape|untaint|width|window)\\b',
         pat: /.+/igm,
         rep: '<span id="reserved">$&</span>'
       },
       {
-        custom: /\b(Array|Date|eval|function|hasOwnProperty|Infinity|isFinite|isNaN|isPrototypeOf|length|Math|NaN|name|Number|Object|prototype|String|toString|undefined|valueOf)\b/igm,
+        custom: '\\b(Array|Date|eval|function|hasOwnProperty|Infinity|isFinite|isNaN|isPrototypeOf|length|Math|NaN|name|Number|Object|prototype|String|toString|undefined|valueOf)\\b',
         pat: /.+/igm,
         rep: '<span id="reserved">$&</span>'
       },
       {
-        custom: /\b(onblur|onclick|onerror|onfocus|onkeydown|onkeypress|onkeyup|onmouseover|onload|onmouseup|onmousedown|onsubmit)\b/igm,
+        custom: '\\b(onblur|onclick|onerror|onfocus|onkeydown|onkeypress|onkeyup|onmouseover|onload|onmouseup|onmousedown|onsubmit)\\b',
         pat: /.+/igm,
         rep: '<span id="parameter">$&</span>'
       },
       {
-        custom: /\/\/.+/igm,
+        custom: '[\/][\/].+',
         pat: /.+/igm,
         rep: '<span id="comment">$&</span>'
       },
@@ -45,7 +45,7 @@
         rep: '<span id="comment">$&</span>'
       },
       {
-        custom: /([\d]|\-[\d]|\.\d)+/igm,
+        custom: '([\d]|[\-][\d]|[\.][\d])+',
         pat: /.+/igm,
         rep: '<span id="unit">$&</span>'
       },
