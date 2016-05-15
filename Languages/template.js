11c819c89c47a18c09970316bd9a391669856e8f
Alexander Hovy <colorizor@gmail.com>
2016-05-15 17:33:57 +0200

    Update template.js

--- Languages/template.js
+++ Languages/template.js
@@ -9,62 +9,18 @@
     ],
     execute: [
       {
-        begin: {
-          pat: '[\\\"]',
-          exclude: false
-        },
-        end: {
-          pat: '[\\\"]',
-          exclude: false
-        },
+        custom: /(\'(.*?)\'|\"(.*?)\")/igm,
         pat: /.+/igm,
         rep: '<span id="value">$&</span>'
       },
       {
-        begin: {
-          pat: '[\\\']',
-          exclude: false
-        },
-        end: {
-          pat: '[\\\']',
-          exclude: false
-        },
-        pat: /.+/igm,
-        rep: '<span id="value">$&</span>'
-      },
-      {
-        begin: {
-          pat: '\\/\\/',
-          exclude: false
-        },
-        end: {
-          pat: '.+',
-          exclude: false
-        },
+        begin: {pat: '\\/\\/', exclude: false},
+        end: {pat: '.+', exclude: false},
         pat: /.+/igm,
         rep: '<span id="comment">$&</span>'
       },
       {
-        begin: {
-          pat: '[\\\']',
-          exclude: true
-        },
-        end: {
-          pat: '[\\\']',
-          exclude: true
-        },
-        pat: /(\<span(.*?)\>|\<\/span\>)/igm,
-        rep: ''
-      },
-      {
-        begin: {
-          pat: '[\\\"]',
-          exclude: true
-        },
-        end: {
-          pat: '[\\\"]',
-          exclude: true
-        },
+        custom: /(\'(.*?)\'|\"(.*?)\")/igm,
         pat: /(\<span(.*?)\>|\<\/span\>)/igm,
         rep: ''
       }
