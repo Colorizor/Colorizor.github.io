270671458df68db6cead9ee2b092b8c5b4729980
Alexander Hovy <colorizor@gmail.com>
2016-05-16 19:05:47 +0200

    Update number.js

--- Plugins/number.js
+++ Plugins/number.js
@@ -1,22 +1,19 @@
-//------------------------------------------------------------------------------------------------------------
-      //---------------------------------------------------NUMBERING----------------------------------------------------
-      //------------------------------------------------------------------------------------------------------------
-      $.each($('pre[number]').find('code'), function() {
-        var str = $(this).html();
-        str = str.replace(/([\s\S]+)/igm, '<span id="numbers"></span><span id="coding">$&</span>');
-        $(this).html(str);
-      });
-      
-      $.each($('pre[number]').find('span[id="coding"]'), function(line) {
-        $(this).html(function(index, html) {
-          return html.replace(/(^\n|.+)/igm, '<span id="code">$&</span>');
-        });
-    
-        line = 0;
-    
-        $.each($(this).find('span[id="code"]'), function() {
-          line++;
-          var str = $($(this).parent().parent().find('span[id="numbers"]')).html();
-          $($(this).parent().parent().find('span[id="numbers"]')).html(str + '<span id="number">' + line + '</span>\n');
-        });
-      });
+$.each($('pre[number]').find('code'), function() {
+var str = $(this).html();
+str = str.replace(/([\s\S]+)/igm, '<span id="numbers"></span><span id="coding">$&</span>');
+$(this).html(str);
+});
+
+$.each($('pre[number]').find('span[id="coding"]'), function(line) {
+$(this).html(function(index, html) {
+return html.replace(/(^\n|.+)/igm, '<span id="code">$&</span>');
+});
+
+line = 0;
+
+$.each($(this).find('span[id="code"]'), function() {
+line++;
+var str = $($(this).parent().parent().find('span[id="numbers"]')).html();
+$($(this).parent().parent().find('span[id="numbers"]')).html(str + '<span id="number">' + line + '</span>\n');
+});
+});
