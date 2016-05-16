9eb5b5118fdf51ba5d8a50d88cc255a285df6c0f
Alexander Hovy <colorizor@gmail.com>
2016-05-16 21:33:33 +0200

    Update number.js

--- Plugins/number.js
+++ Plugins/number.js
@@ -3,7 +3,7 @@
 //================================================================================
 $.each($('pre:not([nonumber])').find('code'), function() {
   var string = $(this).html();
-  string = string.replace(/([\s\S]+)/igm, '<span id="numbers"></span>$&');
+  string = string.replace(/([\s\S]+)/igm, '<span id="numbers"></span><span id="coding">$&</span>');
   $(this).html(string);
 });
 
@@ -15,29 +15,3 @@ $.each($('pre:not([nonumber])').find('code'), function() {
     $(this).find('span[id="numbers"]').html(string + '<span id="number">' + (a + 1) + '</span>\n');
   }
 });
-
-
-/*
-//================================================================================
-//=====================================Number=====================================
-//================================================================================
-$.each($('pre:not([nonumber])').find('code'), function() {
-  var str = $(this).html();
-  str = str.replace(/([\s\S]+)/igm, '<span id="numbers"></span><span id="coding">$&</span>');
-  $(this).html(str);
-});
-
-$.each($('pre:not([nonumber])').find('span[id="coding"]'), function(line) {
-  $(this).html(function(index, html) {
-    return html.replace(/(^\n|.+)/igm, '<span id="code">$&</span>');
-  });
-
-  line = 0;
-
-  $.each($(this).find('span[id="code"]'), function() {
-    line++;
-    var str = $($(this).parent().parent().find('span[id="numbers"]')).html();
-    $($(this).parent().parent().find('span[id="numbers"]')).html(str + '<span id="number">' + line + '</span>\n');
-  });
-});
-*/
