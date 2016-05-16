240f0e91f3560c374bdaf10165726a3058435496
Alexander Hovy <colorizor@gmail.com>
2016-05-16 21:35:03 +0200

    Update colorizor.dev.js

--- colorizor.dev.js
+++ colorizor.dev.js
@@ -424,19 +424,6 @@ var clz = (function() {
         });
       }
     });
-    //Selection
-    $('code').click(function() {
-      if (document.selection) {
-        var block = document.body.createTextRange();
-        block.moveToElementText($(this)[0]);
-        block.select();
-      } else {
-        var block = document.createRange();
-        block.setStartBefore($(this)[0]);
-        block.setEndAfter($(this)[0]);
-        window.getSelection().addRange(block);
-      }
-    });
   }
   
   //================================================================================
