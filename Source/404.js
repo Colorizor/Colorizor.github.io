6e0d6ab3fbc3db3a893c6bd3cc383942e3b29ac9
Alexander Hovy <colorizor@gmail.com>
2016-05-17 08:40:03 +0200

    Update 404.js

--- Source/404.js
+++ Source/404.js
@@ -1,5 +1,5 @@
-//--------------------------------------------------LOADING MAIN
 (function(header) {
+  //Configure
   if (screen.width < 480) {
     header = 150;
   } else if (screen.width >= 480 && screen.width < 720) {
@@ -9,17 +9,64 @@
   } else {
     header = 400;
   }
-
+  
+  //Sizing
   $('header').css({
     'height': header + 'px'
   });
-  
   $('#title-parent').css({
     'height': header + 'px'
   });
-  
   $('footer').css({
     'height': (screen.height - header) + 'px',
     'top': header + 'px'
   });
+  
+  //Social
+  function getFacebook() {
+    $.getJSON('https://graph.facebook.com/fql?q=SELECT%20like_count,%20total_count,%20share_count,%20click_count,%20comment_count%20FROM%20link_stat%20WHERE%20url%20=%20%22https://colorizor.github.io%22', function(data) {
+      var count = data.data[0].total_count;
+      if (count !== '') {
+        $('span[id="facebook"]').text(count);
+      } else {
+        $('span[id="facebook"]').text('Share');
+      }
+    });
+  }
+  function getTwitter() {
+    $.getJSON('https://cdn.api.twitter.com/1/urls/count.json?url=https://colorizor.github.io&callback=?', function(data) {
+      var count = data.count;
+      if (count !== '') {
+        $('span[id="twitter"]').text(count);
+      } else {
+        $('span[id="twitter"]').text('Share');
+      }
+    });
+  }
+  function getPinterest() {
+    $.getJSON('https://api.pinterest.com/v1/urls/count.json?callback%20&url=https://colorizor.github.io', function(data) {
+      var count = data.count;
+      if (count !== '') {
+        $('span[id="pinterest"]').text(count);
+      } else {
+        $('span[id="pinterest"]').text('Share');
+      }
+    });
+  }
+  function getLinkedIn() {
+    $.getJSON('https://www.linkedin.com/countserv/count/share?url=https://colorizor.github.io&callback=?', function(data) {
+      var count = data.count;
+      if (count !== '') {
+        $('span[id="linkedin"]').text(count);
+      } else {
+        $('span[id="linkedin"]').text('Share');
+      }
+    });
+  }
+  
+  //Call
+  getFacebook();
+  getTwitter();
+  getPinterest();
+  getLinkedIn();
 })();
