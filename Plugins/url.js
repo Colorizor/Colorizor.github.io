95426a28e385408a3eae20ee74e79d362959ab71
Alexander Hovy <colorizor@gmail.com>
2016-05-17 20:59:55 +0200

    Update url.js

--- Plugins/url.js
+++ Plugins/url.js
@@ -3,7 +3,7 @@
 //================================================================================
 $.each($('pre:not([nourl])').find('code'), function() {
   var block = $(this).html();
-  block = block.replace(/\[(.*?)\]\[(.*?)\]/igm, function(match) {
+  block = block.replace(/\[(.*?)\]\[(?=(.*?)(ftp|https|http)\:)(.*?)\]/igm, function(match) {
     var split = match.split('][');
     var title = $.trim(split[0].split('[')[1]);
     var link = $.trim(split[1].split(']')[0]);
