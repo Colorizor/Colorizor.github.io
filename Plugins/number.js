bac66e12185f09a1f17f676389244aa65d4e6e14
Alexander Hovy <colorizor@gmail.com>
2016-05-17 16:46:57 +0200

    Update number.js

--- Plugins/number.js
+++ Plugins/number.js
@@ -1,12 +1,6 @@
 //================================================================================
 //=====================================Number=====================================
 //================================================================================
-/*$.each($('pre:not([nonumber])').find('code'), function() {
-  var string = $(this).html();
-  string = string.replace(/([\s\S]+)/igm, '<table id="numbering">$&</span>');
-  $(this).html(string);
-});*/
-
 $.each($('pre:not([nonumber])').find('code'), function() {
   var lines = $(this).html().split('\n');
   var string = '<table id="numbering">';
@@ -18,18 +12,3 @@ $.each($('pre:not([nonumber])').find('code'), function() {
   string += '</table>';
   $(this).html(string);
 });
-
-/*$.each($('pre:not([nonumber])').find('code'), function() {
-  var string = $(this).html();
-  string = string.replace(/([\s\S]+)/igm, '<span id="numbers"></span><span id="coding">$&</span>');
-  $(this).html(string);
-});
-
-$.each($('pre:not([nonumber])').find('code'), function() {
-  var lines = $(this).find('span[id="coding"]').html().split('\n');
-  
-  for (var a = 0; a < lines.length; a++) {
-    var string = $(this).find('span[id="numbers"]').html();
-    $(this).find('span[id="numbers"]').html(string + '<span id="number">' + (a + 1) + '</span>\n');
-  }
-});*/
