b72fbeb50909c737e2b827905506bdff97470d49
Alexander Hovy <colorizor@gmail.com>
2016-05-17 14:05:43 +0200

    Update apache.js

--- Languages/apache.js
+++ Languages/apache.js
@@ -1,4 +1,92 @@
 (function() {
+  const data = {
+    language: '',
+    prepare: [
+      {
+        pat: /\:\/\//gm,
+        rep: '\:\/\\\/'
+      }
+    ],
+    execute: [
+      {
+        custom: '([\'](.*?)[\']|[\"](.*?)[\"])',
+        pat: /.+/gm,
+        rep: '<span id="value">$&</span>'
+      },
+      {
+        keyword: /\b(order|deny|allow|setenv|rewriterule|rewriteengine|rewritecond|documentroot|sethandler|errordocument|loadmodule|options|header|listen|serverroot|servername|on|off|all)\b/gm,
+        rep: '<span id="attribute">$&</span>'
+      },
+      {
+        begin: {pat: '([\$\%][\{])', exclude: true},
+        end: {pat: '[\}]', exclude: true},
+        pat: /([\w]+)/gm,
+        rep: '<span id="selector">$&</span>'
+      },
+      {
+        begin: {pat: '\\s[\\[]', exclude: true},
+        end: {pat: '[\\]]', exclude: true},
+        pat: /([\w]+)/gm,
+        rep: '<span id="parameter">$&</span>'
+      },
+      {
+        custom: '[\#].+',
+        pat: /.+/gm,
+        rep: '<span id="comment">$&</span>'
+      },
+      {
+        custom: '[\$\%]\\d+',
+        pat: /.+/gm,
+        rep: '<span id="unit">$&</span>'
+      },
+      {
+        custom: '[\/](?!([\*]|span))(.*?)(?![\*])[\/](?!span)([igmuy]+|)',
+        pat: /.+/gm,
+        rep: '<span id="regex">$&</span>'
+      },
+      {
+        custom: '[\/](?!([\*]|span))(.*?)(?![\*])[\/](?!span)([igmuy]+|)',
+        pat: /(\<span(.*?)\>|\<\/span\>)/gm,
+        rep: ''
+      },
+      {
+        begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\']', exclude: true},
+        end: {pat: '[\'][\<][\/]span[\>]', exclude: true},
+        pat: /(\<span(.*?)\>|\<\/span\>)/gm,
+        rep: ''
+      },
+      {
+        begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\"]', exclude: true},
+        end: {pat: '[\"][\<][\/]span[\>]', exclude: true},
+        pat: /(\<span(.*?)\>|\<\/span\>)/gm,
+        rep: ''
+      },
+      {
+        begin: {pat: '[\<]span\\sid[\=][\"]comment[\"][\>][\/][\/]', exclude: true},
+        end: {pat: '[\<][\/]span[\>]$', exclude: true},
+        pat: /(\<span(.*?)\>|\<\/span\>)/gm,
+        rep: ''
+      },
+      {
+        begin: {pat: '[\<]span\\sid[\=][\"]comment[\"][\>][\/][\*]', exclude: true},
+        end: {pat: '[\*][\/][\<][\/]span[\>]', exclude: true},
+        pat: /(\<span(.*?)\>|\<\/span\>)/gm,
+        rep: ''
+      }
+    ],
+    finalise: [
+      {
+        pat: /\:\/\\\//gm,
+        rep: '\:\/\/'
+      }
+    ]
+  };
+  clz.Colorize(data);
+})();
+
+
+
+(function() {
   //------------------------------------------------------------------------------------------------------------
   //---------------------------------------------------RegEx----------------------------------------------------
   //------------------------------------------------------------------------------------------------------------
