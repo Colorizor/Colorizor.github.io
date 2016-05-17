2587fcbb8e86fa7a5fd1024069442c93d0114c61
Alexander Hovy <colorizor@gmail.com>
2016-05-17 10:55:09 +0200

    Update 404.js

--- Source/404.js
+++ Source/404.js
@@ -26,7 +26,7 @@
   function getFacebook() {
     $.getJSON('https://graph.facebook.com/fql?q=SELECT%20like_count,%20total_count,%20share_count,%20click_count,%20comment_count%20FROM%20link_stat%20WHERE%20url%20=%20%22https://colorizor.github.io%22', function(data) {
       var count = data.data[0].total_count;
-      if (count !== '') {
+      if (count != '') {
         $('span#facebook').text(count);
       } else {
         $('span#facebook').text('Share');
@@ -36,7 +36,7 @@
   function getTwitter() {
     $.getJSON('https://cdn.api.twitter.com/1/urls/count.json?url=https://colorizor.github.io&callback=?', function(data) {
       var count = data.count;
-      if (count !== '') {
+      if (count != '') {
         $('span#twitter').text(count);
       } else {
         $('span#twitter').text('Share');
@@ -46,7 +46,7 @@
   function getPinterest() {
     $.getJSON('https://api.pinterest.com/v1/urls/count.json?callback%20&url=https://colorizor.github.io', function(data) {
       var count = data.count;
-      if (count !== '') {
+      if (count != '') {
         $('span#pinterest').text(count);
       } else {
         $('span#pinterest').text('Share');
@@ -56,7 +56,7 @@
   function getLinkedIn() {
     $.getJSON('https://www.linkedin.com/countserv/count/share?url=https://colorizor.github.io&callback=?', function(data) {
       var count = data.count;
-      if (count !== '') {
+      if (count != '') {
         $('span#linkedin').text(count);
       } else {
         $('span#linkedin').text('Share');
