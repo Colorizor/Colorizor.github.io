9e0f3f02533a9dc5288af50c99aceeb59af226bb
Alexander Hovy <colorizor@gmail.com>
2016-05-19 15:10:18 +0200

    Update colorize.js

--- Test/colorize.js
+++ Test/colorize.js
@@ -1,9 +1,23 @@
 (function() {
   const data = {
     language: 'colorize',
-    prepare: [],
-    execute: [],
-    finalise: []
+    execute: [
+      {
+        custom: '([\'](.*?)[\']|[\"](.*?)[\"])',
+        pat: /.+/gm,
+        rep: '<span id="string">$&</span>'
+      },
+      {
+        custom: '[\:][\:].+',
+        pat: /.+/gm,
+        rep: '<span id="comment">$&</span>'
+      },
+      {
+        custom: '[\:]([\\w]+)',
+        pat: /([\w]+)/gm,
+        rep: '<span id="keywork">$&</span>'
+      }
+    ]
   };
   clz.Colorize(data);
 })();
