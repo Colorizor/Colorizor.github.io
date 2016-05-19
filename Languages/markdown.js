16c80f321fa2f18e2e2c6bc020607562947936d6
Alexander Hovy <colorizor@gmail.com>
2016-05-19 17:02:40 +0200

    Update markdown.js

--- Languages/markdown.js
+++ Languages/markdown.js
@@ -14,8 +14,8 @@
         rep: '<span id="value">$&</span>'
       },
       {
-        begin: {pat: '&lt;(|[\/])', exclude: true},
-        end: {pat: '(|[\/])&gt;', exclude: true},
+        begin: {pat: '<(|[\/])', exclude: true},
+        end: {pat: '(|[\/])>', exclude: true},
         pat: /^([\w]+)/gm,
         rep: '<span id="selector">$&</span>'
       },
