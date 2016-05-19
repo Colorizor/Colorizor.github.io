31645ec9428ef4ebad835d44dd3d2ab4b429c93a
Alexander Hovy <colorizor@gmail.com>
2016-05-19 15:55:52 +0200

    Update markup.js

--- Languages/markup.js
+++ Languages/markup.js
@@ -8,8 +8,8 @@
         rep: '<span id="value">$&</span>'
       },
       {
-        begin: {pat: '[\&]lt[\;](|[\/])', exclude: true},
-        end: {pat: '(|[\/])[\&]gt[\;]', exclude: true},
+        begin: {pat: '\&lt\;(|[\/])', exclude: true},
+        end: {pat: '(|[\/])\&gt\;', exclude: true},
         pat: /^([\w]+)/gm,
         rep: '<span id="selector">$&</span>'
       },
@@ -20,13 +20,13 @@
         rep: '<span id="attribute">$&</span>'
       },
       {
-        begin: {pat: '[\&]lt[\;](|[\/])', exclude: true},
-        end: {pat: '(|[\/])[\&]gt[\;]', exclude: true},
-        pat: /([\s])([\w\-]+)([^\S\=]|(|[\/])[\&]gt[\;])/gm,
+        begin: {pat: '\&lt\;(|[\/])', exclude: true},
+        end: {pat: '(|[\/])\&gt\;', exclude: true},
+        pat: /([\s])([\w\-]+)([^\S\=]|(|[\/])\&gt\;)/gm,
         rep: '<span id="reserved">$&</span>'
       },
       {
-        custom: '([\&]lt[\;][\!][\-][\-])([\\s\\S]*?)([\-][\-][\&]gt[\;])',
+        custom: '(\&lt\;[\!][\-][\-])([\\s\\S]*?)([\-][\-]\&gt\;)',
         pat: /([\s\S]+)/gm,
         rep: '<span id="comment">$&</span>'
       },
@@ -43,20 +43,20 @@
         rep: ''
       },
       {
-        begin: {pat: '[\<]span\\sid[\=][\"]comment[\"][\>]([\&]lt[\;][\!][\-][\-])', exclude: true},
-        end: {pat: '([\-][\-][\&]gt[\;])[\<][\/]span[\>]', exclude: true},
+        begin: {pat: '[\<]span\\sid[\=][\"]comment[\"][\>](\&lt\;[\!][\-][\-])', exclude: true},
+        end: {pat: '([\-][\-]\&gt\;)[\<][\/]span[\>]', exclude: true},
         pat: /(\<span(.*?)\>|\<\/span\>)/gm,
         rep: ''
       },
       {
-        begin: {pat: '[\&]lt[\;]script(.*?)[\&]gt[\;]', exclude: true},
-        end: {pat: '[\&]lt[\;][\/]script[\&]gt[\;]', exclude: true},
+        begin: {pat: '\&lt\;script(.*?)\&gt\;', exclude: true},
+        end: {pat: '\&lt\;[\/]script\&gt\;', exclude: true},
         pat: /(\<span(.*?)\>|\<\/span\>)/gm,
         rep: ''
       },
       {
-        begin: {pat: '[\&]lt[\;]link(.*?)[\&]gt[\;]', exclude: true},
-        end: {pat: '[\&]lt[\;][\/]link[\&]gt[\;]', exclude: true},
+        begin: {pat: '\&lt\;link(.*?)\&gt\;', exclude: true},
+        end: {pat: '\&lt\;[\/]link\&gt\;', exclude: true},
         pat: /(\<span(.*?)\>|\<\/span\>)/gm,
         rep: ''
       }
