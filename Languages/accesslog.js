cb3ceba1de17e71bce317a6b6e373b96abbca8de
Alexander Hovy <colorizor@gmail.com>
2016-05-19 12:33:00 +0200

    Update accesslog.js

--- Languages/accesslog.js
+++ Languages/accesslog.js
@@ -23,6 +23,18 @@
         rep: '<span id="unit">$&</span>'
       },
       {
+        begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\']', exclude: true},
+        end: {pat: '[\'][\<][\/]span[\>]', exclude: true},
+        pat: /(\<span(.*?)\>|\<\/span\>)/gm,
+        rep: ''
+      },
+      {
+        begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\"]', exclude: true},
+        end: {pat: '[\"][\<][\/]span[\>]', exclude: true},
+        pat: /(\<span(.*?)\>|\<\/span\>)/gm,
+        rep: ''
+      },
+      {
         begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\\\[]', exclude: true},
         end: {pat: '[\\\]][\<][\/]span[\>]', exclude: true},
         pat: /(\<span(.*?)\>|\<\/span\>)/gm,
