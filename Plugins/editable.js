861aabdd8398ddd08f0209a6953147df4b645b53
Alexander Hovy <colorizor@gmail.com>
2016-05-20 16:17:04 +0200

    Update editable.js

--- Plugins/editable.js
+++ Plugins/editable.js
@@ -3,7 +3,7 @@
 //================================================================================
 $('pre:not([noeditable])').dblclick(function() {
   var editable = $(this).attr('contenteditable');
-  if (!editable || editable != true) {
+  if (!editable || editable != 'true') {
     $(this).attr('contenteditable', 'true');
   } else {
     $(this).attr('contenteditable', 'false');
