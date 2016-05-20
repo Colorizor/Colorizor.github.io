4f694b1ea2375fe336d79c07127226ed52bda5f2
Alexander Hovy <colorizor@gmail.com>
2016-05-20 22:10:46 +0200

    Update number.js

--- Plugins/number.js
+++ Plugins/number.js
@@ -1,21 +1,6 @@
 //================================================================================
 //=====================================Number=====================================
 //================================================================================
-/*$.each($('pre:not([nonumber])').find('code'), function() {
-  var lines = $(this).html().split('\n');
-  var block = '<table id="number">';
-  for (var a = 0; a < lines.length; a++) {
-    if (lines[a] != '') {
-      block += '<tr><td id="unselect">' + (a + 1) + '</td><td id="select">' + lines[a] + '</td></tr>';
-    } else {
-      block += '<tr><td id="unselect">' + (a + 1) + '</td><td id="select"> </td></tr>';
-    }
-  }
-  block += '</table>';
-  $(this).html(block);
-});
-*/
-
 $.each($('pre:not([nonumber])'), function() {
   var block = $(this).html(),
       line = $(this).find('code').html(),
