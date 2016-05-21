b0a9dfee20faf8772dd7224a924bb20ab667c86e
Alexander Hovy <colorizor@gmail.com>
2016-05-21 12:45:55 +0200

    Update apache.js

--- Languages/apache.js
+++ Languages/apache.js
@@ -40,11 +40,6 @@
         rep: '<span id="unit">$&</span>'
       },
       {
-        custom: '[\/](?!([\*]|span))(.*?)(?![\*])[\/](?!span)([igmuy]+|)',
-        pat: /(\<span(.*?)\>|\<\/span\>)/gm,
-        rep: ''
-      },
-      {
         begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\']', exclude: true},
         end: {pat: '[\'][\<][\/]span[\>]', exclude: true},
         pat: /(\<span(.*?)\>|\<\/span\>)/gm,
