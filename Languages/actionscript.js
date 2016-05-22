37a092ffaa231bad7c91b64e54e5aaf6b0dca4a1
Alexander Hovy <colorizor@gmail.com>
2016-05-22 19:25:32 +0200

    Update actionscript.js

--- Languages/actionscript.js
+++ Languages/actionscript.js
@@ -28,6 +28,11 @@
         rep: '<span id="selector">$&</span>'
       },
       {
+        custom: '([\-]?)(\\b[0][Xx][a-fA-F\\d]+|([\#]?\\b[\\d]+([\.][\\d]*)?\\b|[\.][\\d]+)([Ee][\-\+]?[\\d]+)?)([\%]|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|pt|pc|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx)?',
+        pat: /.+/gm,
+        rep: '<span id="unit">$&</span>'
+      },
+      {
         custom: '[\/][\/].+',
         pat: /([\s\S]+)/gm,
         rep: '<span id="comment">$&</span>'
@@ -36,11 +41,6 @@
         nested: 'comment',
         begin: '[\/][\*]',
         end: '[\*][\/]'
-      },
-      {
-        custom: '([\-]?)(\\b[0][Xx][a-fA-F\\d]+|([\#]?\\b[\\d]+([\.][\\d]*)?\\b|[\.][\\d]+)([Ee][\-\+]?[\\d]+)?)([\%]|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|pt|pc|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx)?',
-        pat: /.+/gm,
-        rep: '<span id="unit">$&</span>'
       },
       {
         begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\']', exclude: true},
