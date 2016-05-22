6e104412e001a6af279a84ff4332d527eef93c7e
Alexander Hovy <colorizor@gmail.com>
2016-05-22 16:37:13 +0200

    Update colour.js

--- Plugins/colour.js
+++ Plugins/colour.js
@@ -3,6 +3,9 @@
 //================================================================================
 $.each($('pre:not([nocolour])').find('code'), function() {
   var block = $(this).html();
+  block = block.replace(/[RrHh][GgSs][BbLl][Aa]?[\(](.*?)[\)]/gm, function(match) {
+    return match.replace(/(\<span(.*?)\>|\<\/span\>)/gm, '');
+  });
   block = block.replace(/[\#]([a-fA-F\d]{6}|[a-fA-F\d]{3})/gm, '<span style="color:$&;">$&</span>');
   block = block.replace(/[Rr][Gg][Bb][\(](((([\d]{1,3})[\,]{0,1})[\s]*){3})[\)]/gm, '<span style="color:$&;">$&</span>');
   block = block.replace(/[Rr][Gg][Bb][Aa][\(](((([\d]{1,3}|[\d\.]{1,3})[\,]{0,1})[\s]*){4})[\)]/gm, '<span style="color:$&;">$&</span>');
