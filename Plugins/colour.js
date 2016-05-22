25a9e56e2482158577d859757c7c7d5dd1e95d14
Alexander Hovy <colorizor@gmail.com>
2016-05-22 11:18:54 +0200

    Create colour.js

---
+++ Plugins/colour.js
@@ -0,0 +1,18 @@
+//================================================================================
+//=====================================Colour=====================================
+//================================================================================
+$('pre:not([nocolour])').find('code').click(function() {
+  var select = $(this).attr('contenteditable');
+  if (!select || select == 'false') {
+    if (document.selection) {
+      var block = document.body.createTextRange();
+      block.moveToElementText($(this)[0]);
+      block.select();
+    } else {
+      var block = document.createRange();
+      block.setStartBefore($(this)[0]);
+      block.setEndAfter($(this)[0]);
+      window.getSelection().addRange(block);
+    }
+  }
+});
