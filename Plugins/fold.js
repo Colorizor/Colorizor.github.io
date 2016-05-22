bb5f41bbeb0f52a3190b50010d95614a3a699f5e
Alexander Hovy <colorizor@gmail.com>
2016-05-22 19:10:14 +0200

    Update fold.js

--- Plugins/fold.js
+++ Plugins/fold.js
@@ -5,7 +5,7 @@ $('pre:not([nofold])').find('span#comment').click(function() {
   $.each($(this), function() {
     if (!$(this).attr('block') || $(this).attr('block') == '') {
       $(this).attr('block', $(this).html());
-      $(this).html('...');
+      $(this).html(' ... ');
     } else {
       $(this).html($(this).attr('block'));
       $(this).removeAttr('block');
