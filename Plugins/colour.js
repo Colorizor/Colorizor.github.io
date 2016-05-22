018857d7ae36d6029803532bc6a0f7b516f6f4bd
Alexander Hovy <colorizor@gmail.com>
2016-05-22 13:08:14 +0200

    Update colour.js

--- Plugins/colour.js
+++ Plugins/colour.js
@@ -3,10 +3,10 @@
 //================================================================================
 $.each($('pre:not([nocolour])').find('code'), function() {
   var block = $(this).html();
-  block.replace(/[\#]([a-fA-F\d]{6}|[a-fA-F\d]{3})/gm, '<span style="color: $&">$&</span>');
-  block.replace(/[Rr][Gg][Bb][\(](((([\d]{1,3})[\,]{0,1})[\s]*){3})[\)]/gm, '<span style="color: $&">$&</span>');
-  block.replace(/[Rr][Gg][Bb][Aa][\(](((([\d]{1,3}|[\d\.]{1,3})[\,]{0,1})[\s]*){4})[\)]/gm, '<span style="color: $&">$&</span>');
-  block.replace(/[Hh][Ss][Ll][\(](((([\d]{1,3}|[\d\%]{2,4})[\,]{0,1})[\s]*){3})[\)]/gm, '<span style="color: $&">$&</span>');
-  block.replace(/[Hh][Ss][Ll][Aa][\(](((([\d]{1,3}|[\d\%]{2,4}|[\d\.]{1,3})[\,]{0,1})[\s]*){4})[\)]/gm, '<span style="color: $&">$&</span>');
+  block = block.replace(/[\#]([a-fA-F\d]{6}|[a-fA-F\d]{3})/gm, '<span style="color:$&;">$&</span>');
+  block = block.replace(/[Rr][Gg][Bb][\(](((([\d]{1,3})[\,]{0,1})[\s]*){3})[\)]/gm, '<span style="color:$&;">$&</span>');
+  block = block.replace(/[Rr][Gg][Bb][Aa][\(](((([\d]{1,3}|[\d\.]{1,3})[\,]{0,1})[\s]*){4})[\)]/gm, '<span style="color:$&;">$&</span>');
+  block = block.replace(/[Hh][Ss][Ll][\(](((([\d]{1,3}|[\d\%]{2,4})[\,]{0,1})[\s]*){3})[\)]/gm, '<span style="color:$&;">$&</span>');
+  block = block.replace(/[Hh][Ss][Ll][Aa][\(](((([\d]{1,3}|[\d\%]{2,4}|[\d\.]{1,3})[\,]{0,1})[\s]*){4})[\)]/gm, '<span style="color:$&;">$&</span>');
   $(this).html(block);
 });
