1707128cf6dc31ae4d12af75662b4412574428a9
Alexander Hovy <colorizor@gmail.com>
2016-05-22 11:57:30 +0200

    Update colour.js

--- Plugins/colour.js
+++ Plugins/colour.js
@@ -1,18 +1,12 @@
 //================================================================================
 //=====================================Colour=====================================
 //================================================================================
-$('pre:not([nocolour])').find('code').click(function() {
-  var select = $(this).attr('contenteditable');
-  if (!select || select == 'false') {
-    if (document.selection) {
-      var block = document.body.createTextRange();
-      block.moveToElementText($(this)[0]);
-      block.select();
-    } else {
-      var block = document.createRange();
-      block.setStartBefore($(this)[0]);
-      block.setEndAfter($(this)[0]);
-      window.getSelection().addRange(block);
-    }
-  }
+$.each($('pre:not([nocolour])').find('code'), function() {
+  var block = $(this).html();
+  block.replace(/[\#]([a-fA-F\d]{6}|[a-fA-F\d]{3})/gm, '<span style="color:$&;">$&</span>');
+  block.replace(/[Rr][Gg][Bb][\(](((([\d]{1,3})[\,]{0,1})[\s]*){3})[\)]/gm, '<span style="color:$&;">$&</span>');
+  block.replace(/[Rr][Gg][Bb][Aa][\(](((([\d]{1,3}|[\d\.]{1,3})[\,]{0,1})[\s]*){4})[\)]/gm, '<span style="color:$&;">$&</span>');
+  block.replace(/[Hh][Ss][Ll][\(](((([\d]{1,3}|[\d\%]{2,4})[\,]{0,1})[\s]*){3})[\)]/gm, '<span style="color:$&;">$&</span>');
+  block.replace(/[Hh][Ss][Ll][Aa][\(](((([\d]{1,3}|[\d\%]{2,4}|[\d\.]{1,3})[\,]{0,1})[\s]*){4})[\)]/gm, '<span style="color:$&;">$&</span>');
+  $(this).html(block);
 });
