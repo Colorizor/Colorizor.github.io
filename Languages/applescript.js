9166abbfb77b4547b0015744cba46eafed7804ca
Alexander Hovy <colorizor@gmail.com>
2016-05-23 20:07:53 +0200

    Update applescript.js

--- Languages/applescript.js
+++ Languages/applescript.js
@@ -29,19 +29,17 @@
         rep: '<span id="parameter">$&</span>'
       },
       {
-        custom: '[\-][\-].+',
-        pat: /([\s\S]+)/gm,
+        keyword: /([\-]?)(\b[0][Xx][a-fA-F\d]+|([\#]?\b[\d]+([\.][\d]*)?\b|[\.][\d]+)([Ee][\-\+]?[\d]+)?)([\%]|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|pt|pc|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx)?/gm,
+        rep: '<span id="unit">$&</span>'
+      },
+      {
+        keyword: /[\-][\-].+/gm,
         rep: '<span id="comment">$&</span>'
       },
       {
         nested: 'comment',
         begin: '[\(][\*]',
         end: '[\*][\)]'
-      },
-      {
-        custom: '([\-]?)(\\b[0][Xx][a-fA-F\\d]+|([\#]?\\b[\\d]+([\.][\\d]*)?\\b|[\.][\\d]+)([Ee][\-\+]?[\\d]+)?)([\%]|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|pt|pc|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx)?',
-        pat: /.+/gm,
-        rep: '<span id="unit">$&</span>'
       },
       {
         begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\']', exclude: true},
