0a14b80594625033de97866f184eb7bb63e91aa6
Alexander Hovy <colorizor@gmail.com>
2016-05-23 20:10:42 +0200

    Update css.js

--- Languages/css.js
+++ Languages/css.js
@@ -35,8 +35,7 @@
         rep: '<span id="parameter">$&</span>'
       },
       {
-        custom: '([\-]?)(\\b[0][Xx][a-fA-F\\d]+|([\#]?\\b[\\d]+([\.][\\d]*)?\\b|[\.][\\d]+)([Ee][\-\+]?[\\d]+)?)([\%]|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|pt|pc|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx)?',
-        pat: /.+/gm,
+        keyword: /([\-]?)(\b[0][Xx][a-fA-F\d]+|([\#]?\b[\d]+([\.][\d]*)?\b|[\.][\d]+)([Ee][\-\+]?[\d]+)?)([\%]|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|pt|pc|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx)?/gm,
         rep: '<span id="unit">$&</span>'
       },
       {
