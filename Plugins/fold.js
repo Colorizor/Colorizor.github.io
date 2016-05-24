e5ce019d37f305ca58b85691a4eb608331727de1
Alexander Hovy <colorizor@gmail.com>
2016-05-24 19:41:44 +0200

    Update fold.js

--- Plugins/fold.js
+++ Plugins/fold.js
@@ -13,7 +13,7 @@ $('pre:not([nofold])').find('span#block').mousedown(function(event) {
         $(this).html($(this).attr('block'));
         $(this).removeAttr('block');
       }
-      console.log(count);
+      console.log($(this));
       Renumber($(this), count);
       break;
     default:
