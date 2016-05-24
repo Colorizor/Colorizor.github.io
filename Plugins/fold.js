cb7700ddd7bc4b7cfd1102f7d2c4cc6ac323b138
Alexander Hovy <colorizor@gmail.com>
2016-05-24 17:50:25 +0200

    Update fold.js

--- Plugins/fold.js
+++ Plugins/fold.js
@@ -1,12 +1,21 @@
 //================================================================================
 //======================================Fold======================================
 //================================================================================
-$('pre:not([nofold])').find('span#block').dblclick(function() {
-  if (!$(this).attr('block')) {
-    $(this).attr('block', $(this).html());
-    $(this).html(' ... ');
-  } else {
-    $(this).html($(this).attr('block'));
-    $(this).removeAttr('block');
+$('pre:not([nofold])').find('span#block').mousedown(function(event) {
+  $(this).contextmenu(function() {
+    return false;
+  });
+  switch (event.which) {
+    case 1:
+      if (!$(this).attr('block')) {
+        $(this).attr('block', $(this).html());
+        $(this).html('|---|');
+      } else {
+        $(this).html($(this).attr('block'));
+        $(this).removeAttr('block');
+      }
+      break;
+    default:
+      break;
   }
 });
