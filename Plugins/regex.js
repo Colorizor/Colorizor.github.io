71410e43a7efc03afe0b42c5513d6f5bd31b8d9e
Alexander Hovy <colorizor@gmail.com>
2016-05-24 21:14:34 +0200

    Update regex.js

--- Plugins/regex.js
+++ Plugins/regex.js
@@ -3,8 +3,7 @@
 //================================================================================
 $.each($('pre:not([noregex])').find('code'), function() {
   var block = $(this).html(),
-      comment = new RegExp('\<span id\=\"comment\"\>[\/][\/]', 'gm'),
-      value = new RegExp('\<span id\=\"value\"\>(.*?)' + match + '(.*?)\<\/span\>', 'gm');
+      comment = new RegExp('\<span id\=\"comment\"\>[\/][\/]', 'gm');
   block = block.replace(/.+/gm, function(line) {
     var temp = line;
     if (line.match(comment) != null) {
@@ -12,7 +11,7 @@ $.each($('pre:not([noregex])').find('code'), function() {
       temp = temp.replace(/[\/][\/](?![igmuy])(.*?)\<\/span\>$/gm, '<span id="comment">$&');
     }
     return temp.replace(/[\/]([\w\d\S]*?)[\/]([gimuy]+)/gm, function(match) {
-      if (line.match(value) != null) {
+      if (line.match(new RegExp('\<span id\=\"value\"\>(.*?)' + match + '(.*?)\<\/span\>', 'gm')) != null) {
         return match;
       } else {
         return '<span id="regex">' + match + '</span>';
