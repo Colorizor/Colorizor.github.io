74fc8d8a53c7998b5efdca8117c43eab946f833e
Alexander Hovy <colorizor@gmail.com>
2016-05-24 20:43:23 +0200

    Update regex.js

--- Plugins/regex.js
+++ Plugins/regex.js
@@ -1,8 +1,20 @@
 //================================================================================
 //=====================================RegEx======================================
 //================================================================================
-$.each($('pre:not([noregex])'), function() {
+$.each($('pre:not([noregex])').find('code'), function() {
   var block = $(this).html();
-  
+  block = block.replace(/.+/gm, function(line) {
+    return line.replace(/[\/]([\w\d\S]+)[\/]([gimuy]+)?/gm, function(match) {
+      var comment = new RegExp('\<span id\=\"comment\"\>[\/][\/]', 'gm'),
+          value = new RegExp('\<span id\=\"value\"\>(.*?)' + match + '(.*?)\<\/span\>', 'gm');
+      if (line.match(comment) != null) {
+        return match;
+      } else if (line.match(value) != null) {
+        return match;
+      } else {
+        return '<span id="regex">' + match + '</span>';
+      }
+    });
+  });
   $(this).html(block);
 });
