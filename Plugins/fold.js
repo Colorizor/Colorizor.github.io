61d32b216117ba92c0382e96cc568ebf0ccdae70
Alexander Hovy <colorizor@gmail.com>
2016-05-24 19:02:11 +0200

    Update fold.js

--- Plugins/fold.js
+++ Plugins/fold.js
@@ -2,9 +2,6 @@
 //======================================Fold======================================
 //================================================================================
 $('pre:not([nofold])').find('span#block').mousedown(function(event) {
-  $(this).contextmenu(function() {
-    return false;
-  });
   switch (event.which) {
     case 1:
       if (!$(this).attr('block')) {
@@ -14,6 +11,14 @@ $('pre:not([nofold])').find('span#block').mousedown(function(event) {
         $(this).html($(this).attr('block'));
         $(this).removeAttr('block');
       }
+      $.each($(this).parentUntil('pre:not([nonumber])'), function() {
+        var line = $(this).find('code').html(),
+            number = '';
+        for (var a = 0; a < line.split('\n').length; a++) {
+          number += '<span id="number">' + (a + 1) + '</span>\n';
+        }
+        $(this).find('span#numbers').html(number);
+      });
       break;
     default:
       break;
