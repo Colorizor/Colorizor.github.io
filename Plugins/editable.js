9aca39d2ce2020061b3451382e76347fd24b6f68
Alexander Hovy <colorizor@gmail.com>
2016-05-24 17:08:00 +0200

    Update editable.js

--- Plugins/editable.js
+++ Plugins/editable.js
@@ -1,11 +1,28 @@
 //================================================================================
 //====================================Editable====================================
 //================================================================================
-$('pre:not([noeditable])').dblclick(function() {
+/*$('pre:not([noeditable])').find('code').dblclick(function() {
   var editable = $(this).find('code').attr('contenteditable');
   if (!editable || editable != 'true') {
     $(this).find('code').attr('contenteditable', 'true');
   } else {
     $(this).find('code').attr('contenteditable', 'false');
+  }
+});*/
+$('pre:not([noeditable])').find('code').mousedown(function(event) {
+  $(this).contextmenu(function() {
+    return false;
+  });
+  switch (event.which) {
+    case 3:
+      var editable = $(this).attr('contenteditable');
+      if (!editable) {
+        $(this).attr('contenteditable', 'true');
+      } else {
+        $(this).removeAttr('contenteditable');
+      }
+      break;
+    default:
+      break;
   }
 });
