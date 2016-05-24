b6fffe24c298b0641c3a6c21c21e646c21a130eb
Alexander Hovy <colorizor@gmail.com>
2016-05-24 18:45:52 +0200

    Update select.js

--- Plugins/select.js
+++ Plugins/select.js
@@ -1,26 +1,17 @@
 //================================================================================
 //=====================================Select=====================================
 //================================================================================
-$('pre:not([noselect])').mousedown(function(event) {
-  $(this).find('code').contextmenu(function() {
-    return false;
-  });
-  switch (event.which) {
-    case 1:
-      if (!$(this).find('code').attr('contenteditable')) {
-        if (document.selection) {
-          var block = document.body.createTextRange();
-          block.moveToElementText($(this).find('code')[0]);
-          block.select();
-        } else {
-          var block = document.createRange();
-          block.setStartBefore($(this).find('code')[0]);
-          block.setEndAfter($(this).find('code')[0]);
-          window.getSelection().addRange(block);
-        }
-      }
-      break;
-    default:
-      break;
+$('pre:not([noselect])').click(function() {
+  if (!$(this).find('code').attr('contenteditable')) {
+    if (document.selection) {
+      var block = document.body.createTextRange();
+      block.moveToElementText($(this).find('code')[0]);
+      block.select();
+    } else {
+      var block = document.createRange();
+      block.setStartBefore($(this).find('code')[0]);
+      block.setEndAfter($(this).find('code')[0]);
+      window.getSelection().addRange(block);
+    }
   }
 });
