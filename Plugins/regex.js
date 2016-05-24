3a9b9d677a3f480c1e3b0f285e1b5960b0a3a789
Alexander Hovy <colorizor@gmail.com>
2016-05-24 21:11:13 +0200

    Update regex.js

--- Plugins/regex.js
+++ Plugins/regex.js
@@ -2,15 +2,17 @@
 //=====================================RegEx======================================
 //================================================================================
 $.each($('pre:not([noregex])').find('code'), function() {
-  var block = $(this).html();
+  var block = $(this).html(),
+      comment = new RegExp('\<span id\=\"comment\"\>[\/][\/]', 'gm'),
+      value = new RegExp('\<span id\=\"value\"\>(.*?)' + match + '(.*?)\<\/span\>', 'gm');
   block = block.replace(/.+/gm, function(line) {
-    return line.replace(/[\/]([\w\d\S]*?)[\/]([gimuy]+)/gm, function(match) {
-      var comment = new RegExp('\<span id\=\"comment\"\>[\/][\/]', 'gm'),
-          value = new RegExp('\<span id\=\"value\"\>(.*?)' + match + '(.*?)\<\/span\>', 'gm');
-      if (line.match(comment) != null) {
-        var temp = line.replace(comment, '//');
-        return temp.replace(/[\/][\/](?![igmuy])(.*?)\<\/span\>$/gm, '<span id="comment">$&');
-      } else if (line.match(value) != null) {
+    var temp = line;
+    if (line.match(comment) != null) {
+      temp = line.replace(comment, '//');
+      temp = temp.replace(/[\/][\/](?![igmuy])(.*?)\<\/span\>$/gm, '<span id="comment">$&');
+    }
+    return temp.replace(/[\/]([\w\d\S]*?)[\/]([gimuy]+)/gm, function(match) {
+      if (line.match(value) != null) {
         return match;
       } else {
         return '<span id="regex">' + match + '</span>';
