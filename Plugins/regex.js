9a9e35979cbb6b48c0af2508f746b5ceb42cbbe8
Alexander Hovy <colorizor@gmail.com>
2016-05-24 21:04:35 +0200

    Update regex.js

--- Plugins/regex.js
+++ Plugins/regex.js
@@ -4,11 +4,12 @@
 $.each($('pre:not([noregex])').find('code'), function() {
   var block = $(this).html();
   block = block.replace(/.+/gm, function(line) {
-    return line.replace(/[\/]([\w\d\S]+)[\/]([gimuy]+)?/gm, function(match) {
+    return line.replace(/[\/]([\w\d\S]*?)[\/]([gimuy]+)/gm, function(match) {
       var comment = new RegExp('\<span id\=\"comment\"\>[\/][\/]', 'gm'),
           value = new RegExp('\<span id\=\"value\"\>(.*?)' + match + '(.*?)\<\/span\>', 'gm');
       if (line.match(comment) != null) {
-        return match;
+        var temp = line.replace(comment, '//');
+        return temp.replace(/[\/][\/](?![igmuy])(.*?)\<\/span\>$/gm, '<span id="comment">$&');
       } else if (line.match(value) != null) {
         return match;
       } else {
