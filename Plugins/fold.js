a3ccb46d20a08989b214b271c3421c5dfc929768
Alexander Hovy <colorizor@gmail.com>
2016-05-24 14:51:36 +0200

    Update fold.js

--- Plugins/fold.js
+++ Plugins/fold.js
@@ -2,9 +2,7 @@
 //======================================Fold======================================
 //================================================================================
 $('pre:not([nofold])').find('span#block').click(function() {
-  console.log(!$(this).attr('block') == true);
   if (!$(this).attr('block')) {
-    console.log(!$(this).attr('block') == true);
     $(this).attr('block', $(this).html());
     $(this).html(' ... ');
   } else {
