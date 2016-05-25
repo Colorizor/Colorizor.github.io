822c551537e8ae55505dbf04cbb522ae816e90b2
Alexander Hovy <colorizor@gmail.com>
2016-05-25 10:36:54 +0200

    Update keyword.js

--- Plugins/keyword.js
+++ Plugins/keyword.js
@@ -1,6 +1,8 @@
-$.each($('pre:not([nokeyword])').find('code'), function() {
-  var block = $(this).html();
-  block = block.replace(/[\@]clz[\-]/gm, '<span id="none">');
-  block = block.replace(/[\-]clz[\@]/gm, '</span>');
-  $(this).html(block);
+$(window).load(function() {
+  $.each($('pre:not([nokeyword])').find('code'), function() {
+    var block = $(this).html();
+    block = block.replace(/[\@]clz[\-]/gm, '<span id="none">');
+    block = block.replace(/[\-]clz[\@]/gm, '</span>');
+    $(this).html(block);
+  });
 });
