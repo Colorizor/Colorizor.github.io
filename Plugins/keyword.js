d31d53ff7f8d5cc402f44acabb8bf7af673df386
Alexander Hovy <colorizor@gmail.com>
2016-05-25 10:15:49 +0200

    Create keyword.js

---
+++ Plugins/keyword.js
@@ -0,0 +1,6 @@
+$.each($('pre:not([nokeyword])').find('code'), function() {
+  var block = $(this).html();
+  block = block.replace(/[\@]clz/gm, '<span id="none">');
+  block = block.replace(/clz[\@]/gm, '</span>');
+  $(this).html(block);
+});
