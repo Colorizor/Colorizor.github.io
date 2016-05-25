f138eeb6638e2d988413117dad11e656475bfeae
Alexander Hovy <colorizor@gmail.com>
2016-05-25 10:38:57 +0200

    Update url.js

--- Plugins/url.js
+++ Plugins/url.js
@@ -1,13 +1,12 @@
-//================================================================================
-//=======================================URL======================================
-//================================================================================
-$.each($('pre:not([nourl])').find('code'), function() {
-  var block = $(this).html();
-  block = block.replace(/\[(.*?)\]\[(?=(.*?)(ftp|https|http)\:)(.*?)\]/igm, function(match) {
-    var split = match.split('][');
-    var title = split[0].split('[')[1];
-    var link = split[1].split(']')[0];
-    return '<a id="url" href="' + link.trim() + '" target="_blank">' + title.trim() + '</a>';
+$(window).load(function() {
+  $.each($('pre:not([nourl])').find('code'), function() {
+    var block = $(this).html();
+    block = block.replace(/\[(.*?)\]\[(?=(.*?)(ftp|https|http)\:)(.*?)\]/igm, function(match) {
+      var split = match.split('][');
+      var title = split[0].split('[')[1];
+      var link = split[1].split(']')[0];
+      return '<a id="url" href="' + link.trim() + '" target="_blank">' + title.trim() + '</a>';
+    });
+    $(this).html(block);
   });
-  $(this).html(block);
 });
