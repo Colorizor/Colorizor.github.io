95b2913a56247da25df5c7ce1a84256aad3f7438
Alexander Hovy <colorizor@gmail.com>
2016-05-25 10:41:50 +0200

    Update keyword.js

--- Plugins/keyword.js
+++ Plugins/keyword.js
@@ -1,8 +1,6 @@
-$(window).load(function() {
-  $.each($('pre:not([nokeyword])').find('code'), function() {
-    var block = $(this).html();
-    block = block.replace(/[\@]clz[\-]/gm, '<span id="none">');
-    block = block.replace(/[\-]clz[\@]/gm, '</span>');
-    $(this).html(block);
-  });
+$.each($('pre:not([nokeyword])').find('code'), function() {
+  var block = $(this).html();
+  block = block.replace(/[\@]clz[\-]/gm, '<span id="none">');
+  block = block.replace(/[\-]clz[\@]/gm, '</span>');
+  $(this).html(block);
 });
