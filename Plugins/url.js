d07b745c0163a266bb4211156755ade40cb3e4b3
Alexander Hovy <colorizor@gmail.com>
2016-05-26 22:29:07 +0200

    Update url.js

--- Plugins/url.js
+++ Plugins/url.js
@@ -1,6 +1,6 @@
 $.each($('pre:not([nourl])').find('code'), function() {
   var block = $(this).html(),
-  normal = '\\b((ftp|http(s)?)[\:][\/]{2}([\\w\\d\.\/\?\&\=\,\!\@\#\$\%\\\(\\\)\\\[\\\]\:\;\'\"\-\_\+]+)?)\\b',
+  normal = '\\b((ftp|http(s)?)[\:][\/]{2}([\\w\\d\.]+)([\/\?\&\\w\\d\=\,\.\_\-\+\:\@\$\%\#\!]+)?)\\b',
   custom = '[\\\[](.*?)[\\\]][\\\[](?=(.*?)(ftp|http(s)?)[\:])(.*?)[\\\]]';
   block = block.replace(new RegExp('('+custom+'|'+normal+')', 'gm'), function(match) {
     if (match.match(new RegExp('^'+normal+'$', 'gm')) != null) {
