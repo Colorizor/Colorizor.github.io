9252366da506f07fe62e4b6c5dc51f69cf98df02
Alexander Hovy <colorizor@gmail.com>
2016-05-26 15:53:52 +0200

    Update email.js

--- Plugins/email.js
+++ Plugins/email.js
@@ -1,6 +1,5 @@
 $.each($('pre:not([noemail])').find('code'), function() {
   var block = $(this).html();
-  block = block.replace(/\b(([\w\d\.\_\%\+\-]+)[\@]([\w\d\.\-]+)([\.][\w]{2,}))\b/gm, function(match) {
-  });
+  block = block.replace(/\b(([\w\d\.\_\%\+\-]+)[\@]([\w\d\.\-]+)([\.][\w]{2,}))\b/gm, '<a id="url" href="mailto:$&">$&</a>');
   $(this).html(block);
 });
