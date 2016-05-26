fef5526b770a0c0f6074c6b57182f059756f0c22
Alexander Hovy <colorizor@gmail.com>
2016-05-26 16:43:07 +0200

    Update email.js

--- Plugins/email.js
+++ Plugins/email.js
@@ -1,5 +1,16 @@
 $.each($('pre:not([noemail])').find('code'), function() {
-  var block = $(this).html();
-  block = block.replace(/\b(([\w\d\.\_\%\+\-]+)[\@]([\w\d\.\-]+)([\.][\w]{2,}))\b/gm, '<a id="url" href="mailto:$&">$&</a>');
+  var block = $(this).html(),
+  normal = '\\b(([\\w\\d\.\_\%\+\-]+)[\@]([\\w\\d\.\-]+)([\.][\\w]{2,}))\\b',
+  custom = '[\\\[](.*?)[\\\]][\\\[](?=(.*?)([\\w]+)[\@])(.*?)[\\\]]';
+  block = block.replace(new RegExp(`(${custom}|${normal})`, 'gm'), function(match) {
+    if (match.match(new RegExp(`^${normal}$`, 'gm')) != null) {
+      return `<a id="url" href="mailto:${match}">${match}</a>`;
+    } else {
+      var split = match.split('][');
+      var title = split[0].split('[')[1];
+      var email = split[1].split(']')[0];
+      return `<a id="url" href="mailto:${email.trim()}">${title.trim()}</a>`;
+    }
+  });
   $(this).html(block);
 });
