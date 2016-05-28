635041ef5ad345ad6c81386b83c318bc5534cd4b
Alexander Hovy <colorizor@gmail.com>
2016-05-28 15:19:09 +0200

    Update editor.js

--- Plugins/editor.js
+++ Plugins/editor.js
@@ -1,4 +1,10 @@
 $('pre:not([noeditable])').bind('contentchanged', function() {
   var lang = $(this).find('code[contenteditable]').attr('language');
-  window[lang.toLowerCase()]['Editorize']($(this).find('code[contenteditable]'));
+  var func = window[lang.toLowerCase()]['Editorize'];
+  if (typeof func === 'function') {
+    func.apply($(this).find('code[contenteditable]'));
+    console.log('yes');
+  } else {
+    console.log('no');
+  }
 });
