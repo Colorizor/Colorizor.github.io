5c49f54de176ff6213f364e45578eb71a089b4cf
Alexander Hovy <colorizor@gmail.com>
2016-05-28 14:12:59 +0200

    Update axapta.js

--- Languages/axapta.js
+++ Languages/axapta.js
@@ -26,17 +26,17 @@
         rep: '<span id="attribute">$&</span>'
       },
       {
+        custom: '[\(](.*?)[\)]',
+        pat: /([\w]+)/gm,
+        rep: '<span id="parameter">$&</span>'
+      },
+      {
         keyword: /\b(false|int|abstract|private|char|boolean|static|null|if|for|true|while|long|throw|finally|protected|final|return|void|enum|else|break|new|catch|byte|super|case|short|default|double|public|try|this|switch|continue|reverse|firstfast|firstonly|forupdate|nofetch|sum|avg|minof|maxof|count|order|group|by|asc|desc|index|hint|like|dispaly|edit|client|server|ttsbegin|ttscommit|str|real|date|container|anytype|common|div|mod)\b/gm,
         rep: '<span id="reserved">$&</span>'
       },
       {
         keyword: /([\w]+)(?=[\(])/gm,
         rep: '<span id="selector">$&</span>'
-      },
-      {
-        custom: '[\(](.*?)[\)]',
-        pat: /([\w]+)/gm,
-        rep: '<span id="parameter">$&</span>'
       },
       {
         keyword: /([\-]?)(\b[0][Xx][a-fA-F\d]+|([\#]?\b[\d]+([\.][\d]*)?\b|[\.][\d]+)([Ee][\-\+]?[\d]+)?)([\%]|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|pt|pc|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx)?/gm,
