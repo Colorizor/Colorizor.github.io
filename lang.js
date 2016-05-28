b2535d414df7850969f304bfc811c9070b36056b
Alexander Hovy <colorizor@gmail.com>
2016-05-28 14:38:53 +0200

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,128 @@
+(function() {
+  const data = {
+    language: 'javascript',
+    prepare: [
+      {
+        pat: /\:\/\//gm,
+        rep: '\:\/\\\/'
+      },
+      {
+        pat: /[\\][\']/gm,
+        rep: '$SQ$'
+      },
+      {
+        pat: /[\\][\"]/gm,
+        rep: '$DQ$'
+      },
+      {
+        pat: /[\$]\&amp\;/gm,
+        rep: '$FR@'
+      }
+    ],
+    execute: [
+      {
+        custom: '[\`]([\\s\\S]*?)[\`]',
+        pat: /[\$][\{](.*?)[\}]{1,}/gm,
+        rep: '\`$&\`'
+      },
+      {
+        keyword: /([\`](.*?)[\`]|[\'](.*?)[\']|[\"](.*?)[\"])/gm,
+        rep: '<span id="value">$&</span>'
+      },
+      {
+        keyword: /\b(abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\b/gm,
+        rep: '<span id="attribute">$&</span>'
+      },
+      {
+        keyword: /\b(alert|all|anchor|anchors|area|assign|blur|button|checkbox|clearInterval|clearTimeout|clientInformation|close|closed|confirm|constructor|crypto|decodeURI|decodeURIComponent|defaultStatus|document|element|elements|embed|embeds|encodeURI|encodeURIComponent|escape|event|fileUpload|focus|form|forms|frame|height|innerHeight|innerWidth|layer|layers|link|location|mimeTypes|navigate|navigator|frames|frameRate|hidden|history|image|images|offscreenBuffering|open|opener|option|outerHeight|outerWidth|packages|pageXOffset|pageYOffset|parent|parseFloat|parseInt|password|plugin|prompt|propertyIsEnum|radio|reset|screen|screenX|screenY|scroll|secure|select|self|setInterval|setTimeout|status|submit|taint|text|textarea|top|unescape|untaint|width|window)\b/gm,
+        rep: '<span id="reserved">$&</span>'
+      },
+      {
+        keyword: /\b(Array|Date|eval|function|hasOwnProperty|Infinity|isFinite|isNaN|isPrototypeOf|length|Math|NaN|name|Number|Object|prototype|String|toString|undefined|valueOf)\b/gm,
+        rep: '<span id="reserved">$&</span>'
+      },
+      {
+        keyword: /\b(onblur|onclick|onerror|onfocus|onkeydown|onkeypress|onkeyup|onmouseover|onload|onmouseup|onmousedown|onsubmit)\b/gm,
+        rep: '<span id="parameter">$&</span>'
+      },
+      {
+        begin: {pat: '[\.](?=([\\w]+)((.?)[\=]|[\(]))', exclude: true},
+        end: {pat: '([\=]|[\(])', exclude: true},
+        pat: /([\w]+)/gm,
+        rep: '<span id="selector">$&</span>'
+      },
+      {
+        keyword: /([\-]?)(\b[0][Xx][a-fA-F\d]+|([\#]?\b[\d]+([\.][\d]*)?\b|[\.][\d]+)([Ee][\-\+]?[\d]+)?)([\%]|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|pt|pc|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx)?/gm,
+        rep: '<span id="unit">$&</span>'
+      },
+      {
+        keyword: /[\/][\/].+/gm,
+        rep: '<span id="comment">$&</span>'
+      },
+      {
+        nested: 'comment',
+        begin: '[\/][\*]',
+        end: '[\*][\/]'
+      },
+      {
+        begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\`]', exclude: true},
+        end: {pat: '[\`][\<][\/]span[\>]', exclude: true},
+        pat: /\<(\/)?span(.*?)\>/gm,
+        rep: ''
+      },
+      {
+        begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\']', exclude: true},
+        end: {pat: '[\'][\<][\/]span[\>]', exclude: true},
+        pat: /\<(\/)?span(.*?)\>/gm,
+        rep: ''
+      },
+      {
+        begin: {pat: '[\<]span\\sid[\=][\"]value[\"][\>][\"]', exclude: true},
+        end: {pat: '[\"][\<][\/]span[\>]', exclude: true},
+        pat: /\<(\/)?span(.*?)\>/gm,
+        rep: ''
+      },
+      {
+        custom: '[\`][\<][\/]span[\>][\$][\{]',
+        pat: /[\`][\<][\/]span[\>]/gm,
+        rep: '<\/span>'
+      },
+      {
+        custom: '[\}][\<]span(.*?)[\>][\`]',
+        pat: /[\<]span(.*?)[\>][\`]/gm,
+        rep: '<span id="value">'
+      },
+      {
+        begin: {pat: '[\<]span\\sid[\=][\"]comment[\"][\>][\/][\/]', exclude: true},
+        end: {pat: '[\<][\/]span[\>]$', exclude: true},
+        pat: /\<(\/)?span(.*?)\>/gm,
+        rep: ''
+      },
+      {
+        begin: {pat: '([\/][\*])\<span\\sid\=\"block\"\>', exclude: true},
+        end: {pat: '\<\/span\>([\*][\/])', exclude: true},
+        pat: /\<(\/)?span(.*?)\>/gm,
+        rep: ''
+      }
+    ],
+    finalise: [
+      {
+        pat: /\:\/\\\//gm,
+        rep: '\:\/\/'
+      },
+      {
+        pat: /[\$][S][Q][\$]/gm,
+        rep: '\\\''
+      },
+      {
+        pat: /[\$][D][Q][\$]/gm,
+        rep: '\\\"'
+      },
+      {
+        pat: /[\$][F][R][\@]/gm,
+        rep: '&#36;\&'
+      }
+    ]
+  };
+  clz.Colorize(data);
+})();
