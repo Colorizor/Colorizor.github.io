a4ebb1fa22773b43084c3659dcbc88d3957e3794
Alexander Hovy <colorizor@gmail.com>
2016-05-29 22:41:22 +0200

    Update colorizor.js

--- colorizor.js
+++ colorizor.js
@@ -99,7 +99,7 @@ var clz = (function() {
     code = $(object).html().replace(/\<(\/)?span(.*?)\>/gm, '');
   }
   function Save(object) {
-    $(object).html(Trim(code));
+    $(object).html(code);
   }
   //==============================Core
   function Prepare() {
