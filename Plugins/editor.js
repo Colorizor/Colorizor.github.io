036c257e955ee453584ddc15c01816d6f0640f3f
Alexander Hovy <colorizor@gmail.com>
2016-05-29 13:52:22 +0200

    Update editor.js

--- Plugins/editor.js
+++ Plugins/editor.js
@@ -1,7 +1,27 @@
-$('pre:not([noeditor])').keydown(function() {
+$('pre:not([noeditor])').keyup(function() {
   var lang = $(this).find('code[contenteditable]').attr('language');
   var func = window[lang.toLowerCase()]['Editorize'];
   if (typeof func === 'function') {
     func.apply(null, $(this).find('code[contenteditable]'));
+    Cursor($(this));
   }
 });
+function Cursor(object) {
+  var offset = 0;
+  object = $(object).get(0);
+  if (document.selection) {
+    var range = document.selection.createRange();
+    var caret = document.body.createRange();
+    caret.moveToElementText(object);
+    caret.setEndPoint('EndToEnd', range);
+    offset = caret.text.length;
+  } else {
+    var range = window.getSelection().getRangeAt(0);
+    var caret = range.cloneRange();
+    caret.selectNodeContents(object);
+    caret.setEnd(range.endContainer, range.endOffset);
+    offset = caret.toString().length;
+  }
+  alert(offset);
+}
+//^([^\S\n]){1,}(?=[\S])
