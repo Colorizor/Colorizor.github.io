69210c1837c82f009022772aa3495261cfef80d8
Alexander Hovy <colorizor@gmail.com>
2016-05-29 23:58:24 +0200

    Update email.js

--- Plugins/email.js
+++ Plugins/email.js
@@ -9,9 +9,9 @@ var email = (function() {
           if (match.match(new RegExp('^'+normal+'$', 'gm')) != null) {
             return '<a id="url" href="mailto:'+match+'">'+match+'</a>';
           } else {
-            var split = match.split('][');
-            var title = split[0].split('[')[1];
-            var email = split[1].split(']')[0];
+            var split = match.split(']['),
+                title = split[0].split('[')[1],
+                email = split[1].split(']')[0];
             return '<a id="url" href="mailto:'+email.trim()+'">'+title.trim()+'</a>';
           }
         });
@@ -26,9 +26,9 @@ var email = (function() {
         if (match.match(new RegExp('^'+normal+'$', 'gm')) != null) {
           return '<a id="url" href="mailto:'+match+'">'+match+'</a>';
         } else {
-          var split = match.split('][');
-          var title = split[0].split('[')[1];
-          var email = split[1].split(']')[0];
+          var split = match.split(']['),
+              title = split[0].split('[')[1],
+              email = split[1].split(']')[0];
           return '<a id="url" href="mailto:'+email.trim()+'">'+title.trim()+'</a>';
         }
       });
