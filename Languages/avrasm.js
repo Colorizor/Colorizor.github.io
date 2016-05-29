bc966264f6132b1c60803620ce6aad9096c8eead
Alexander Hovy <colorizor@gmail.com>
2016-05-29 22:35:20 +0200

    Update avrasm.js

--- Languages/avrasm.js
+++ Languages/avrasm.js
@@ -1,4 +1,4 @@
-(function() {
+var avrasm = (function() {
   const data = {
     language: 'avrasm',
     prepare: [
@@ -89,5 +89,13 @@
       }
     ]
   };
-  clz.Colorize(data);
+  return {
+    Colorize: function() {
+      clz.Colorize(data);
+    },
+    Editorize: function(object) {
+      clz.Colorize(data, $(object));
+    }
+  };
 })();
+avrasm.Colorize();
