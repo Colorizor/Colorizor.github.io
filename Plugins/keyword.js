a69f4d52466e41df96f148d0b0d3cab0873ce2f7
Alexander Hovy <colorizor@gmail.com>
2016-05-29 23:51:58 +0200

    Update keyword.js

--- Plugins/keyword.js
+++ Plugins/keyword.js
@@ -1,6 +1,19 @@
-$.each($('pre:not([nokeyword])').find('code'), function() {
-  var block = $(this).html();
-  block = block.replace(/[\@]clz[\-]/gm, '<span id="none">');
-  block = block.replace(/[\-]clz[\@]/gm, '</span>');
-  $(this).html(block);
-});
+var keyword = (function() {
+  return {
+    Init: function() {
+      $.each($('pre:not([nokeyword])').find('code'), function() {
+        var block = $(this).html();
+        block = block.replace(/[\@]clz[\-]/gm, '<span id="none">');
+        block = block.replace(/[\-]clz[\@]/gm, '</span>');
+        $(this).html(block);
+      });
+    },
+    Colorize: function(object) {
+      var block = $(object).find('code').html();
+      block = block.replace(/[\@]clz[\-]/gm, '<span id="none">');
+      block = block.replace(/[\-]clz[\@]/gm, '</span>');
+      $(object).find('code').html(block);
+    }
+  };
+})();
+keyword.Init();
