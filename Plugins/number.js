6da50db0498554b0f4290ffca5d4195091500878
Alexander Hovy <colorizor@gmail.com>
2016-05-29 23:01:14 +0200

    Update number.js

--- Plugins/number.js
+++ Plugins/number.js
@@ -1,10 +1,14 @@
-$.each($('pre:not([nonumber])'), function() {
-  var block = $(this).html(),
-      line = $(this).find('code').html(),
-      number = '';
-  for (var a = 0; a < line.split('\n').length; a++) {
-    number += '<span id="number">'+(a + 1)+'</span>\n';
-  }
-  block = '<span id="numbers">'+number+'</span><span id="coding">'+block+'</span>';
-  $(this).html(block);
-});
+var number = (function(object) {
+  return {
+    Colorize: function() {
+      var block = $(object).html(),
+          line = $(object).find('code').html(),
+          number = '';
+      for (var a = 0; a < line.split('\n').length; a++) {
+        number += '<span id="number">'+(a + 1)+'</span>\n';
+      }
+      block = '<span id="numbers">'+number+'</span><span id="coding">'+block+'</span>';
+      $(object).html(block);
+    }
+  };
+})();
