6b9d877ce5022cbee27d1e55d42efd7a5ca5b4c4
Alexander Hovy <colorizor@gmail.com>
2016-05-29 15:35:00 +0200

    Update editor.js

--- Plugins/editor.js
+++ Plugins/editor.js
@@ -8,39 +8,41 @@ $('pre:not([noeditor])').keyup(function() {
   }
 });
 var editor = (function() {
-  GetCursor: function(object) {
-    var offset = 0;
-    var element = $(object).get(0);
-    if (document.selection) {
-      var range = document.selection.createRange();
-      var caret = document.body.createRange();
-      caret.moveToElementText(element);
-      caret.setEndPoint('EndToEnd', range);
-      offset = caret.text.length;
-    } else {
-      var range = window.getSelection().getRangeAt(0);
-      var caret = range.cloneRange();
-      caret.selectNodeContents(element);
-      caret.setEnd(range.endContainer, range.endOffset);
-      offset = caret.toString().length;
+  return {
+    GetCursor: function(object) {
+      var offset = 0;
+      var element = $(object).get(0);
+      if (document.selection) {
+        var range = document.selection.createRange();
+        var caret = document.body.createRange();
+        caret.moveToElementText(element);
+        caret.setEndPoint('EndToEnd', range);
+        offset = caret.text.length;
+      } else {
+        var range = window.getSelection().getRangeAt(0);
+        var caret = range.cloneRange();
+        caret.selectNodeContents(element);
+        caret.setEnd(range.endContainer, range.endOffset);
+        offset = caret.toString().length;
+      }
+      $(object).attr('cursor', offset.toString());
+    },
+    SetCursor: function(object) {
+      var offset = parseInt($(object).attr('cursor'), 10);
+      var element = $(object).get(0);
+      if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.moveStart('character', offset);
+        range.moveEnd('character', offset);
+        range.select();
+      } else {
+        var range = document.createRange();
+        range.setStartAfter(element, offset);
+        range.setEndAfter(element, offset);
+        window.getSelection().addRange(range);
+      }
     }
-    $(object).attr('cursor', offset.toString());
-  }
-  SetCursor: function(object) {
-    var offset = parseInt($(object).attr('cursor'), 10);
-    var element = $(object).get(0);
-    if (document.selection) {
-      var range = document.body.createTextRange();
-      range.moveToElementText(element);
-      range.moveStart('character', offset);
-      range.moveEnd('character', offset);
-      range.select();
-    } else {
-      var range = document.createRange();
-      range.setStartAfter(element, offset);
-      range.setEndAfter(element, offset);
-      window.getSelection().addRange(range);
-    }
-  }
-  //^([^\S\n]){1,}(?=[\S])
+  };
 })();
+//^([^\S\n]){1,}(?=[\S])
