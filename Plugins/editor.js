36a3d701127d6b9f27a4d877c5ead2499259d992
Alexander Hovy <colorizor@gmail.com>
2016-05-29 17:28:24 +0200

    Update editor.js

--- Plugins/editor.js
+++ Plugins/editor.js
@@ -1,6 +1,6 @@
 $('pre:not([noeditor])').keyup(function() {
-  var lang = $(this).find('code[contenteditable]').attr('language');
-  var func = window[lang.toLowerCase()]['Editorize'];
+  var lang = $(this).find('code[contenteditable]').attr('language'),
+      func = window[lang.toLowerCase()]['Editorize'];
   if (typeof func === 'function') {
     GetCursor($(this).find('code[contenteditable]'));
     func.apply(null, $(this).find('code[contenteditable]'));
@@ -8,17 +8,17 @@ $('pre:not([noeditor])').keyup(function() {
   }
 });
 function GetCursor(object) {
-  var offset = 0;
-  var element = $(object).get(0);
+  var offset = 0,
+      element = $(object).get(0);
   if (document.selection) {
-    var range = document.selection.createRange();
-    var caret = document.body.createRange();
+    var range = document.selection.createRange(),
+        caret = document.body.createRange();
     caret.moveToElementText(element);
     caret.setEndPoint('EndToEnd', range);
     offset = caret.text.length;
   } else {
-    var range = window.getSelection().getRangeAt(0);
-    var caret = range.cloneRange();
+    var range = window.getSelection().getRangeAt(0),
+        caret = range.cloneRange();
     caret.selectNodeContents(element);
     caret.setEnd(range.endContainer, range.endOffset);
     offset = caret.toString().length;
@@ -26,20 +26,48 @@ function GetCursor(object) {
   $(object).attr('cursor', offset.toString());
 }
 function SetCursor(object) {
-  var offset = parseInt($(object).attr('cursor'), 10);
-  var element = $(object).get(0);
+  var offset = parseInt($(object).attr('cursor'), 10),
+      element = $(object).get(0),
+      nodes = [];
+  function Nodes(current) {
+    for (var a = 0; a < current.childNodes.length; a++) {
+      var child = current.childNodes[a];
+      if (child.nodeType == 3) {
+        nodes.push(child.nodeValue);
+      } else {
+        Nodes(child);
+      }
+    }
+  }
   if (document.selection) {
     var range = document.body.createTextRange();
     range.moveToElementText(element);
-    range.moveStart('character', offset);
+    range.collapse(true);
     range.moveEnd('character', offset);
+    range.moveStart('character', offset);
     range.select();
   } else {
-    var range = document.createRange();
-    console.log(element);
-    range.setStartAfter(element, offset);
-    range.setEndAfter(element, offset);
-    window.getSelection().addRange(range);
+    Nodes(element);
+    var range = document.createRange(),
+        count = 0,
+        endCount = 0,
+        started = false;
+    range.collapse(true);
+    range.selectNodeContents(element);
+    for (var a = 0; a < nodes.length; a++) {
+      var node = nodes[a];
+      endCount = count + node.length;
+      if (!started && offset >= count && (offset < endCount || (offset == endCount && a <= nodes.length))) {
+        range.setStart(node, offset - count);
+        started = true;
+      }
+      if (started && offset <= endCount) {
+        range.setEnd(node, offset - count);
+        break;
+      }
+      count = endCount;
+    }
+    window.getSelection().removeAllRanges().addRange(range);
   }
 }
 //^([^\S\n]){1,}(?=[\S])
